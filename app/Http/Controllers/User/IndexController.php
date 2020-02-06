3b996b6645b1a7ac4a84e2768599ba921d9c9482
13166560446 <2256006378@qq.com>
2020-02-06 09:11:26 +0800

    防刷鉴权

---
+++ app/Http/Controllers/User/IndexController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\UserModel;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Str;
+class IndexController extends Controller
+{
+        public function index(Request $request){
+            $name=$request->input('name');
+            $email=$request->input('email');
+            $tel=$request->input('tel');
+            $pass1=$request->input('pass1');
+            $pass2=$request->input('pass2');
+
+            if($pass1!=$pass2){
+                die('输入的两次密码不一致');die;
+            }
+
+            $pattern="/([a-z0-9]*[-_.]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[.][a-z]{2,3}([.][a-z]{2})?/i";
+            if(!preg_match($pattern,$email)){
+                echo '邮箱格式错误！';die;
+            }
+
+            $data=[
+                'name'=>$name,
+                'email'=>$email,
+                'tel'=>$tel,
+                'pass'=>$pass1
+            ];
+//            dd($data);
+            $res=UserModel::insertGetId($data);
+            if($res){
+                echo '注册成功';
+            }else{
+                echo '注册失败';die;
+            }
+
+        }
+        public function login(Request $request){
+            $data=$request->all();
+            $res=UserModel::where($data)->first();
+            if($res){
+                echo "登录成功";
+                $token=Str::random(32);
+                $key=$res->id.":";
+                Redis::set($key,$token,40000);
+                $response=[
+                    'errno'=>0,
+                    'msg'=>'ok',
+                    'data'=>[
+                        'token'=>$token
+                    ]
+                ];
+//                return $response;
+            }else{
+                echo "登录失败";
+                $response=[
+                    'errno'=>40002,
+                    'msg'=>'密码不正确'
+                ];
+            }
+            return $response;
+        }
+        public function userlist(){
+//           print_r($_SERVER);die;
+            $name=$_SERVER['HTTP_NAME'];
+            $token =$_SERVER['HTTP_TOKEN'];
+//          echo  $name=$_SERVER['NAME'];
+            $res=UserModel::where(['name'=>$name])->first();
+            $key=$res->id.":";
+//            echo $key;die;
+            $info=Redis::get($key);
+//            echo $info;die;
+            if($info==$token){
+                echo "<pre>";print_r($res);echo"</pre>";
+            }else{
+                echo "请输入正确的token";
+            }
+        }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/index/index','User\IndexController@index');
+Route::post('/index/login','User\IndexController@login');
+Route::get('/index/userlist','User\IndexController@userlist');
+
